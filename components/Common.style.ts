150542ec862064425074530ce635bfc34f0756ee
JHYOOOOON <jhyooooooon@gmail.com>
2022-08-07 16:45:27 +0900

    style: 스타일 수정

--- components/Common.style.ts
+++ components/Common.style.ts
@@ -1,10 +1,10 @@
 import styled from "@emotion/styled";
 
 export const SectionTitle = styled.h1`
-	line-height: 1.15;
+	line-height: 1.25;
 	margin-bottom: 0;
 	color: var(--border-point);
-	font-size: 45px;
+	font-size: 35px;
 	font-weight: 700;
 `;
 
