1efa363df00e9b4ab30c66619f782ec24cc3d959
jhyoon <jhyooooooon@gmail.com>
2022-11-05 12:10:05 +0900

    chore: google analytics 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,49 @@
-import { AppProps } from 'next/app'
-import '../styles/globals.css'
+import { AppProps } from "next/app";
+import { useEffect } from "react";
+import Script from "next/script";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	const router = useRouter();
+	useEffect(() => {
+		const handleRouteChange = (url: any) => {
+			gtag.pageview(url);
+		};
+		router.events.on("routeChangeComplete", handleRouteChange);
+		router.events.on("hashChangeComplete", handleRouteChange);
+		return () => {
+			router.events.off("routeChangeComplete", handleRouteChange);
+			router.events.off("hashChangeComplete", handleRouteChange);
+		};
+	}, [router.events]);
+
+	return (
+		<>
+			{/* Global Site Tag (gtag.js) - Google Analytics */}
+			<Script
+				strategy="afterInteractive"
+				src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+			/>
+			<Script
+				id="gtag-init"
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{
+					__html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${gtag.GA_TRACKING_ID}', {
+          page_path: window.location.pathname,
+        });
+      `,
+				}}
+			/>
+			{/* gtag End */}
+			<Component {...pageProps} />
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
